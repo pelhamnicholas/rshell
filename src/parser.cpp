64602caf3f494e4b4617c4cccfad306d16c980a0
Nicholas Pelham <pelhamnicholas@gmail.com>
2015-11-20 03:01:52 -0800

    Memory problem fixed.

--- src/parser.cpp
+++ src/parser.cpp
@@ -18,51 +18,28 @@ char ** Parser::get_cstrTokens() {
     // get size
     int size = distance(tokLine->begin(), tokLine->end()) + 1;
     
-    /*
-    // free memory is neccesary
-    if (cToken != NULL) {
-        for (int i = 0, cToken[i] != NULL, ++i)
-            free(cToken[i]);
-        free(cToken);
-    }
-    */
-
     // try catch
-    cStr = (char **) malloc(size * sizeof(char*));
+    try {
+        cStr = (char **) malloc(size * sizeof(char*));
+    } catch (...) {
+        // Error handling
+    }
 
     int j = 0;
     for (so_tokenizer::iterator i = tokLine->begin(); 
             i != tokLine->end(); ++i) {
-        // try catch
-        cStr[j] = (char*) malloc(sizeof((*i).c_str()));
-        strcpy(cStr[j++], (*i).c_str());
+
+        try {
+            cStr[j] = (char*) malloc(strlen((*i).c_str()));//sizeof((*i).c_str()));
+            strcpy(cStr[j++], (*i).c_str());
+        } catch (...) {
+            //cout << "Error allocating memory for individual cstring";
+            //cout << endl;
+        }
     }
 
     return cStr;
 }
-
-/*
-char ** Parser::get_cstrTokens(so_tokenizer::iterator i) {
-    char ** cStr;
-
-    //get size
-    int size = 0;
-    for (so_tokenizer::iterator j = i; j != tokLine.end() 
-            && !isConnector(*j) && !isComment(*j); ++j)
-        size++;
-
-    cStr = (char**) malloc((size+1) * sizeof(char*));
-    int k = 0;
-    for (so_tokenize::iterator j = i; j != tokLine.end()
-            && !isConnector(*j) && !isComment(*j); ++j) {
-        cStr[k] = (char*) malloc(sizeof((*j).cStr()));
-        strcpy(cStr[k++], (*j).c_str());
-    }
-    cStr[k] = NULL;
-
-    return cStr;
-}
-*/
 
 char ** Parser::tokenize(const string & s) {
     if (tokLine != NULL)
